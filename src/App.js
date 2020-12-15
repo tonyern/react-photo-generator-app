16996d9ef552b399761a56dd3a9588e7b11b478c
Tony <tony.d.nguyen@protonmail.com>
2020-12-14 18:40:41 -0600

    Successfully connected to the API and can generate new faces.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const generatedPhotoApi = {
+    key: "A5CyUWr5cryc9KOdcJvqJw",
+    base: "https://api.generated.photos/api/v1/"
+  }
+
+  const [image, setImage] = useState('');
+
+  const generatePhoto = () => {
+    console.log('Generating new photo...');
+
+    fetch(`${generatedPhotoApi.base}faces?api_key=${generatedPhotoApi.key}&gender=female&order_by=random`)
+    .then(res => res.json())
+    .then(result => {
+      console.log(result.faces[0].urls[4][512]);
+      setImage(result.faces[0].urls[4][512]);
+    });
+  };
+
   return (
     <div className="App">
       <h1>Black Market Photo Generator</h1>
+      
+      {(typeof image != "undefined") ? (
+        <img src={image} alt="Face for Black Market use" width="512px" height="512px" />
+      ) : ('')}
+
+      <br />
+      <br />
+
+      <button onClick={generatePhoto}>Generate New Photo</button>
     </div>
   );
 }
